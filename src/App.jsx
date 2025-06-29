963bbb841b7b6ea4683c138f1eb4c91697e7783b
Hitesh11112 <hiteshpatwal2004@gmail.com>
2025-06-29 16:54:40 +0530

    converted each function to arrow function, added todo list memory array

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,51 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0);
-  const Increment = () => {
-    if (count >= 20) return;
-    setCount(count + 1);
+  const [todo, setTodo] = useState([
+    { title: "title 1", description: "description 1" },
+    { title: "title 2", description: "description 2" },
+    { title: "title 3", description: "description 3" }
+  ]);
+  const addtodo = () => {
+    setTodo([...todo, {
+      title: "title 4",
+      description: "description 4"
+    }]);
   };
-  const Decrement = () => {
-    if (count <= 0) return;
-    setCount(count - 1);
-  };
+  
   return (
     <>
-      <button style={{ backgroundColor: 'yellow', color: 'black' }} onClick={Increment}>Increment</button>
-      <div className="card">
-        <p>Count is {count}</p>
-      </div>
-      <button style={{ backgroundColor: 'yellow', color: 'black' }} onClick={Decrement}>Decrement</button>
+      <Counter />
+      <button onClick={addtodo}>Add to do</button>
+      {todo.map((item, index) => <Header key={index} title={item.title} description={item.description} />)}
     </>
   )
 }
+
+const Header = ({ title, description }) => {
+  return (
+    <>
+      <h1>{title}</h1>
+      <h3>{description}</h3>
+    </>
+  );
+};
+
+
+const Counter = () => {
+  const [count, setCount] = useState(0);
+  const Increment = () => { if (count >= 20) return; setCount(count + 1); };
+  const Decrement = () => { if (count <= 0) return; setCount(count - 1); };
+
+  return (
+    <>
+      <button onClick={Increment}>Increment</button>
+      <p>Count is {count}</p>
+      <button onClick={Decrement}>Decrement</button>
+      <br /><br />
+    </>
+  );
+};
+
 
 export default App
