a53ee62dcb44c34b55117009d95f316087150016
Hitesh11112 <hiteshpatwal2004@gmail.com>
2025-06-30 09:27:00 +0530

    added component folder with jsx files

---
+++ components/Header.jsx
@@ -0,0 +1,12 @@
+
+const Header = ({ title, description }) => {
+  return (
+    <>
+      <h1>{title}</h1>
+      <h3>{description}</h3>
+    </>
+  );
+};
+
+
+export default Header
--- src/App.jsx
+++ src/App.jsx
@@ -23,30 +23,4 @@ function App() {
   )
 }
 
-const Header = ({ title, description }) => {
-  return (
-    <>
-      <h1>{title}</h1>
-      <h3>{description}</h3>
-    </>
-  );
-};
-
-
-const Counter = () => {
-  const [count, setCount] = useState(0);
-  const Increment = () => { if (count >= 20) return; setCount(count + 1); };
-  const Decrement = () => { if (count <= 0) return; setCount(count - 1); };
-
-  return (
-    <>
-      <button onClick={Increment}>Increment</button>
-      <p>Count is {count}</p>
-      <button onClick={Decrement}>Decrement</button>
-      <br /><br />
-    </>
-  );
-};
-
-
 export default App
